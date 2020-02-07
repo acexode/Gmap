75d721877f5bbca107c85d1a9a14cfac1e6032d2
Acexode <abubakar.abdulwahab@outsourceglobal.com>
2020-02-07 20:19:28 +0100

    init commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {Plugins} from '@capacitor/core'
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable, observable } from 'rxjs';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { LoadingController, NavController } from '@ionic/angular';
+const {Geolocation} = Plugins
+declare var google;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  user = null;
+  location : Observable<any>
+  locationCollection: AngularFirestoreCollection<any>
+  @ViewChild('map', {static: false}) mapElement: ElementRef
+  map: any;
+  markers = [];
+  isPickUpRequested: boolean
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private loading: LoadingController, private nav : NavController ) {
+      this.login()
+      this.isPickUpRequested = false
+  }
 
-  constructor() {}
+  confirmPickup(){
+    this.isPickUpRequested = true
+    console.log(this.isPickUpRequested)
+  }
+  cancelPickup(){
+    this.isPickUpRequested = false
+  }
+
+  ionViewWillEnter(){
+    console.log('object'); 
+    this.getCurrentLocation()
+  }
+  loadMap(lat, lng) {    
+    let latLng = new google.maps.LatLng( lat, lng); 
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+      options: {
+        gestureHandling: 'greedy'
+      }
+    };
+    console.log(this.mapElement.nativeElement);
+ 
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  
+  login(){
+    this.afAuth.auth.signInAnonymously().then(user =>{
+      console.log(user);
+      this.user = user
+      this.locationCollection = this.afs.collection(`location/${this.user.uid}/track`, ref => ref.orderBy('timestamp'))
+    })
+  }
+  async getCurrentLocation(){
+    const loading = await this.loading.create({
+      message: 'Getting your current Location.....',
+      duration: 2000
+    });
+    await loading.present();
+    // let locationObservable = Observable.create(observable =>{      
+      Geolocation.getCurrentPosition({timeout: 10000, enableHighAccuracy:true})
+      .then(resp =>{
+        let lat = resp.coords.latitude
+        let lng = resp.coords.longitude
+        this.loadMap(lat,lng)
+       
+        this.loading.dismiss()
+        
+      }, err =>{
+        console.log('Geolocation err: '+ err);
+      })
+    // })
+    // return locationObservable
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyCK8z-gmGCa4CLRGjfTe2-DL1jAQaEZ2Ts",
+    authDomain: "flow-91ef7.firebaseapp.com",
+    databaseURL: "https://flow-91ef7.firebaseio.com",
+    projectId: "flow-91ef7",
+    storageBucket: "flow-91ef7.appspot.com",
+    messagingSenderId: "373004708637",
+    appId: "1:373004708637:web:d755ce183f9bd53ea3cff4",
+    measurementId: "G-R7E7YS3QNE"
+  }
+  // Initialize Firebase
+  
 };
 
 /*
